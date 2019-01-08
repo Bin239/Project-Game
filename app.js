8c261f40335211bc6f2f4f8b08fed974c716db85
Binod <binod@Binitas-MacBook-Air.local>
2019-01-08 14:16:17 -0700

    added a few more functions and changed pictures

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 console.log("JS working");
 
-// class ghost {
-//     constructor (){
-//         strength = 20,
+class enemy {
+    constructor(){
+        this.strength = 20;
+    }
+}
 
-//     }
-// }
+let firstEnemy = new enemy;
+
+
+class finalEnemy{
+    constructor(){
+        this.strength = 40;
+    }
+}
+
+let finalBoss = new finalEnemy;
+
 
 
 class samurai {
@@ -20,7 +31,7 @@ let mySamurai = new samurai;
 function timeremaining (){
     if(mySamurai.time > 0){
     mySamurai.time --;
-    $(".time").text( "Time Remaining: " + mySamurai.time)
+    $(".time").append(mySamurai.time)
 }}
 let Timedecrease = setInterval(timeremaining, 1000);
 
@@ -33,7 +44,7 @@ let samuraiStrength = setInterval(strengthRemaining, 1000);
 function strengthRemaining(){
     if(mySamurai.strength > 0){
     mySamurai.strength --;
-    $(".strength").text("Strength: " + mySamurai.strength)
+    $(".strength").append(mySamurai.strength);
 }}
 
 function feedSamurai() {
@@ -48,19 +59,62 @@ $(".feedSamurai").on("click", (e) => {
     feedSamurai();
 })
 
+function attack(){
+     $(".samurai").attr("src", "pics/samurai3.gif");
+     setTimeout(resetCharacter, 1700);
+}
+
+function moveLeft(){
+    $(".samurai").stop().animate({
+            left: '-=100'
+        });
+}
+
+function moveright(){
+    $(".samurai").stop().animate({
+            left: '+=100'
+        });
+}
+
+function resetCharacter (){
+    $(".samurai").attr("src", "pics/staticninja.gif")
+}
 
 $(document).keydown(function(e) {
     switch (e.which) {
     case 37:
-        $('.samurai').stop().animate({
-            left: '-=100'
-        }); //left arrow key
-        console.log("key pressed");
+        moveLeft();
+         //left arrow key
         break;
     case 39:
-        $('.samurai').stop().animate({
-            left: '+=100'
-        }); //right arrow key
+        moveright();
+         //right arrow key
+        break;
+    case 32:
+    attack();
+    setInterval(enemyDies, 1300);
         break;
     }
+    
 })
+
+
+function enemyDies () {
+    $(".image3").attr("src", "pics/explode.gif");
+    setTimeout(resetEnemy, 1300);
+}
+
+function resetEnemy(){
+    $(".image3").attr("src", "pics/ghost5.gif", loop = true)
+}
+
+
+
+
+
+
+
+//<img class="samurai" src="pics/staticninja.gif" alt="Samurai" style="width: 300px; height: 180px;">
+//<img class="image4" src="pics/samurai3.gif" alt="samurai gif" style="width: 330px; height: 200px;">
+//<img class="explode" src="pics/explode.gif" alt="explosion" style="width: 70px; height: 80px;"></img>
+
