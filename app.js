f01de79da8a58c0ad802c7aa6f1b51deb24d5416
Binod <binod@Binitas-MacBook-Air.local>
2019-01-11 13:37:59 -0700

    game done

--- app.js
+++ app.js
@@ -24,16 +24,18 @@ class samurai {
     constructor() {
         this.strength = 100,
             this.score = 5;
-        this.time = 60;
+        this.time = 45;
     }
 }
 
 let mySamurai = new samurai();
 
+window.onload = function () {setInterval(enemyAttack, 2000); };
+
 function timeremaining() {
     if (mySamurai.time > 0) {
         mySamurai.time--;
-        $(".time").text("Time Remaining: " + mySamurai.time)
+        $(".time").text("Time Remaining: " + mySamurai.time + " Sec.")
 
 
     }
@@ -56,8 +58,6 @@ function resetEnemy() {
     $("#image3").css("height", "280px");
     $("#image3").css("width", "280px");
     $("#samurai").css("margin-top", "420px");
-
-
 }
 
 function enemyDeadPicture() {
@@ -65,14 +65,24 @@ function enemyDeadPicture() {
 
 }
 
+function enemyAttack (){
+    gameOver();
+    
+    if(mySamurai.strength > 4 &&  $("#samurai").offset().left < 320){
+    mySamurai.strength -=5;
+    }
+        
+}
+
 
 for (let i = 0; i < 3; i++) {
 
     function attack() {
 
-       
+        
 
-        if (($("#samurai").offset().left < 350)) {
+        if (($("#samurai").offset().left < 320)) {
+
 
             $("#score").text("Score: " + mySamurai.score);
 
@@ -88,7 +98,6 @@ for (let i = 0; i < 3; i++) {
         $("#samurai").attr("src", "pics/samurai3.gif");
         
         if (firstEnemy.strength < 1) {
-            //scoreIncrease();
             setTimeout(enemyDeadPicture, 1000);
             clearTimeout(enemyDeadPicture);
             setTimeout(resetEnemy, 1700);
@@ -96,8 +105,6 @@ for (let i = 0; i < 3; i++) {
         }
 
         setTimeout(resetCharacter, 1700);
-        console.log(firstEnemy.strength);
-        console.log(mySamurai.score);
     }
 }
 
@@ -124,6 +131,7 @@ $(document).keydown(function (e) {
         case 37:
             if (($("#samurai").offset().left > 260)) {
                 moveLeft();
+                
             }
             //left arrow key
             break;
@@ -134,18 +142,15 @@ $(document).keydown(function (e) {
             //right arrow key
             break;
         case 32:
-            gameOver();
+            //gameOver();
 
             if (mySamurai.strength > 9) {
                 attack();
             }
-            //setTimeout(gameOver, 2000);
 
-            if (mySamurai.strength > 9) {
-                mySamurai.strength -= 10;
+            if (mySamurai.strength > 4) {
+                mySamurai.strength -= 5;
             }
-
-            //setInterval(enemyDies, 1300);
             break;
         case 38:
             feedSamurai();
@@ -164,9 +169,6 @@ function updateStrengthBar() {
     var id = setInterval(frame, 500);
     function frame() {
 
-        // if(width <= 15){
-        //     $("#strengthProgress").css("background-color", "hsl(14, 78%, 39%)")
-        // }
         if (width <= 0) {
             clearInterval(id);
         }
@@ -189,30 +191,13 @@ function gameOver() {
         $("#samurai").hide();
         $("#image3").hide();
         setTimeout(alertFunction, 300);
+        $("#score").text("Score: " + mySamurai.score);
+
 
     }
 }
 
 function alertFunction() {
-    alert("Gameover, Refresh the page to play again!!");
+    alert("Gameover, your score was " + mySamurai.score + ". Press Ok to play again!!");
+    location.reload();
 }
-
-
-// var $stored_selector = $('#samurai');
-
-// if($($stored_selector.offset().left < 300)){
-   // enemyAttack();
-// }
-
-// console.log($stored_selector.offset());
-
-
-//$stored_selector.offset();
-
-
-
-
-//<img class="samurai" src="pics/staticninja.gif" alt="Samurai" style="width: 300px; height: 180px;">
-//<img class="image4" src="pics/samurai3.gif" alt="samurai gif" style="width: 330px; height: 200px;">
-//<img class="explode" src="pics/explode.gif" alt="explosion" style="width: 70px; height: 80px;"></img>
-
